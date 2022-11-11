ea83b099f9e6d6775f40b3fc6aaa01bde2d18ebb
kmlindgren <kmlindgren@students.unwsp.edu>
2022-11-10 19:24:01 -0600

    Create TwoExceptions

---
+++ in-class-ch-12-assignments/src/TwoExceptions.java
@@ -0,0 +1,17 @@
+
+public class TwoExceptions {
+
+	public static void main(String[] args) {
+		String defAnInt = "This is definitely an integer.";
+		int num = 0;
+		try {
+			System.out.println(5 / num);
+			//Integer.parseInt(defAnInt);
+		} /*catch(NumberFormatException e) {
+			System.out.println("Unable to format this 'integer.'");
+		} */catch (ArithmeticException e) {
+			System.out.println("Unable to perform this arithmetic equation.");
+		}
+	}
+
+}
